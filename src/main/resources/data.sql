98b6a1e78e877a8c33820a948ba4aaa9ae8ca783
ELBEDRI Khalid <Khalid.ELBEDRI@cegedim.com>
2023-11-25 11:19:55 +0100

    Move product initialization to data.sql.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,9 @@ CREATE TABLE IF NOT EXISTS products (
                                         price DECIMAL(10, 2) NOT NULL,
                                         quantity INT NOT NULL
 );
+
+INSERT INTO products (id, name, price, quantity)
+VALUES
+    ('becd5e12-defd-4238-8739-5ac69fb06b0f', 'H8 LED', 30, 11),
+    ('462538de-0cc5-46ed-9a4c-034980242fe9', 'H3 LED', 25, 5),
+    ('a517dec9-e025-4681-9b7d-584ebd5f990a', 'D1S HID', 40, 44);
