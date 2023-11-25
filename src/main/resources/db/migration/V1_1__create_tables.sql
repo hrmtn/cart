f28c625e318619cfb54f86a54b62c4adf9497197
ELBEDRI Khalid <Khalid.ELBEDRI@cegedim.com>
2023-11-25 13:40:45 +0100

    Use postgresql instead of H2.

--- src/main/resources/db/migration/V1_1__create_tables.sql
+++ src/main/resources/db/migration/V1_1__create_tables.sql
@@ -29,9 +29,3 @@ CREATE TABLE IF NOT EXISTS orders_products (
                                                FOREIGN KEY (order_id) REFERENCES orders(id),
                                                FOREIGN KEY (product_id) REFERENCES products(id)
 );
-
-INSERT INTO products (id, name, price, quantity)
-VALUES
-    ('becd5e12-defd-4238-8739-5ac69fb06b0f', 'H8 LED', 30, 11),
-    ('462538de-0cc5-46ed-9a4c-034980242fe9', 'H3 LED', 25, 5),
-    ('a517dec9-e025-4681-9b7d-584ebd5f990a', 'D1S HID', 40, 44);
